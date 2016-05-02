83a7bd2d614a9cdba6bc285cf7ddd493eae7130f
Alexis Vincent <alexis@alexisvincent.io>
2016-05-02 20:05:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,18 +4,8 @@ import { AppContainer } from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Layout from './Layout';
 
-const rootEl = document.getElementById('root');
-
-let render = () =>
-    ReactDOM.render(
-      <AppContainer><App /></AppContainer>,
-      rootEl
-    );
-
-export function __reload() {
-    render();
-}
-
-render();
+ReactDOM.render(
+    <AppContainer><App /></AppContainer>,
+    document.getElementById('root')
+);
